bec3a6d1858681af744001253b882b018f69421e
SaraZ <saraaban@gmail.com>
2019-03-04 16:30:20 -0800

    step2 complete

---
+++ step2.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const axios=require('axios');
+
+function cat(path){
+    fs.readFile(path, 'utf8', function(error, data){
+        if(error){
+            console.error(error);
+            process.exit(1);
+        }
+        else{
+            console.log(`file content: ${data}`);
+        }
+    });
+}
+
+function webCat(url){
+    
+   return axios.get(url)
+    .then(function(response){
+        console.log(response.data);
+    })
+    .catch(function (error){
+        if (error) {
+            console.log(
+            `Error fetching ${url}: 
+            Error: Request failed with status code ${error.response.status}`);
+
+        }
+    });
+}
+
+const address=process.argv[2];
+
+if (address.startsWith("http")) {
+    webCat(address);
+} else {
+    cat(address);
+}
+
